98388c37e7041df7a95e811afd72cc874a9fee0a
haharimi <haharimi@naver.com>
2020-05-18 17:18:37 +0900

    git push

---
+++ js/weather.js
@@ -0,0 +1,50 @@
+const weather = document.querySelector(".js-weather");
+const COORDS = 'coords';
+const API_KEY = "66d632d1a097f94e3d72d426acdef5f6";
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj)); 
+    //localstorage의 key, value 값은 모두 string 타입으로 저장되기때문에 변환시켜준다. number -> string
+}
+
+function handleSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = { // 객체의 key,  value 값이 동일할 때에는 한번만 써줘도 된다.
+        latitude,       // localStorage에 객체로 value에 저장하기위해서 객체에 넣어준다.    
+        longitude
+    };
+    saveCoords(coordsObj); // localStorage에 위치 저장 
+    getWeather(latitude, longitude); // 날씨 가져오기
+
+}
+
+function handleError() {
+    console.log('cant not access to location');
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
+}
+
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) { 
+        // localStorage에 좌표값이 저장되어있지않다면
+        askForCoords(); // 좌표값을 물어본다
+    } else { 
+        //  좌표값이 있다면,
+        getWeather();
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
