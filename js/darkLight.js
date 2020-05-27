8609bf500adda2ba2d6e0b4aca1f68a064a08b76
haharimi <haharimi@naver.com>
2020-05-27 11:55:27 +0900

    다크모드 적용시 테마변경 CSS 적용

--- js/darkLight.js
+++ js/darkLight.js
@@ -2,7 +2,7 @@ const darkLight_cb = document.querySelector(".darkLight_Cb");
 
 function changeHandler () {
     document.body.classList.toggle('dark');
-    document.h1.classList.toggle('dark');
+    
 }
 
 function init () {
